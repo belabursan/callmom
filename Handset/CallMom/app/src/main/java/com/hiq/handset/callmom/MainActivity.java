a73619f3fe975d13dc3f70a38a52359de23f189c
Patrik <patrik.paradis@gmail.com>
2015-02-05 15:17:45 +0100

    Working on the action bar

--- Handset/CallMom/app/src/main/java/com/hiq/handset/callmom/MainActivity.java
+++ Handset/CallMom/app/src/main/java/com/hiq/handset/callmom/MainActivity.java
@@ -28,17 +28,25 @@ public class MainActivity extends ActionBarActivity{
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.options:
+                openOptions();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
+    }
 
-        return super.onOptionsItemSelected(item);
+    /** Called when the user touches the button */
+    private void openOptions() {
+        // Do something in response to button click
+        Context context = getApplicationContext();
+        CharSequence text = "Options pressed!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 
     /** Called when the user touches the button */
