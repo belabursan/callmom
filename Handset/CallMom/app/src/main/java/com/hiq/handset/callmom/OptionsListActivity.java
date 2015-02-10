cb570bdbc92399b207b6789bf386af3b7f9d7196
Patrik <patrik.paradis@gmail.com>
2015-02-10 11:41:44 +0100

    Working on Options

---
+++ Handset/CallMom/app/src/main/java/com/hiq/handset/callmom/OptionsListActivity.java
@@ -0,0 +1,60 @@
+package com.hiq.handset.callmom;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+/**
+ * Created by Patrik on 2015-02-09.
+ */
+public class OptionsListActivity extends ListActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.options_list_activity);
+
+        String[] options = {"Register","Unregister"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, options);
+        setListAdapter(adapter);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // The activity is about to become visible.
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // The activity has become visible (it is now "resumed").
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // Another activity is taking focus (this activity is about to be "paused").
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        // The activity is no longer visible (it is now "stopped")
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // The activity is about to be destroyed.
+    }
+
+    protected void onListItemClick(ListView parent, View v, int position, long id){
+        String item = (String) getListAdapter().getItem(position);
+        Toast.makeText(this, item + " selected", Toast.LENGTH_SHORT).show();
+    }
+}
+
