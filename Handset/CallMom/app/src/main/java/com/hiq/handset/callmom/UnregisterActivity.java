5775d955380ac1e8fc0dfc8de0e2f1d426f5c362
Patrik <patrik.paradis@gmail.com>
2015-02-20 11:21:26 +0100

    Working on Register and Unregister

---
+++ Handset/CallMom/app/src/main/java/com/hiq/handset/callmom/UnregisterActivity.java
@@ -0,0 +1,16 @@
+package com.hiq.handset.callmom;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Patrik on 2015-02-20.
+ */
+public class UnregisterActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.unregister_layout);
+    }
+}
